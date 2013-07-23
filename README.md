66221fe473abe3591002802306d001d78d4ff477
Greg Reinacker <gregr@rassoc.com>
2013-07-23 17:45:31 -0600

    work with multiple NodeBalancers at once

--- README.md
+++ README.md
@@ -4,15 +4,21 @@ This is a command line tool to cover a common use case. Specifically, suppose yo
 
 So using this tool, you can do
 
-    loadbalancer server1 reject
+    loadbalancer balancer1 server1 reject
 
 This will take server1 completely out of rotation in the NodeBalancer, on all ports it is listening on.  To bring it back online, use
 
-    loadbalancer server1 accept
+    loadbalancer balancer1 server1 accept
 
 Which will bring it back.
 
 In both cases, it will loop waiting for the change to actually propogate; when it has completed, and your changes have taken effect, the tool exits.
+
+You can also use this with multiple load balancers at the same time; for example, if you have a server called server1, which is a member node in all of your NodeBalancers, you could use something like
+
+    loadbalancer ALL server1 reject
+
+which will take server1 offline on all NodeBalancers configured on your account.
 
 Uses the most excellent [Linode gem](https://github.com/rick/linode); you'll need to install that gem first using
 
--- loadbalancer.rb
+++ loadbalancer.rb
@@ -2,31 +2,48 @@
 
 require 'linode'
 
-if ARGV.count != 2 || !["accept","reject"].include?(ARGV[1])
-  puts "usage: loadbalancer {host_label} {accept|reject}"
+if ARGV.count != 3 || !["accept","reject"].include?(ARGV[2])
+  puts "usage: loadbalancer {node_balancer_label | ALL} {host_label} {accept|reject}"
   exit
 end
 
-cmd_label = ARGV[0]
-cmd_action = ARGV[1]
+nb_label = ARGV[0]
+cmd_label = ARGV[1]
+cmd_action = ARGV[2]
 
 l = Linode.new(:api_key => "LINODE API KEY HERE")
 
+balancers = []
 nb_list = l.nodebalancer.list
-nb = nb_list.first
-puts "Found NodeBalancer #{nb.label} (#{nb.nodebalancerid})"
+if nb_label == "ALL"
+  balancers = nb_list
+else
+  nb = nb_list.find {|n| n.label == nb_label}
+  if nb
+    balancers << nb
+  else
+    puts "could not find nodebalancer labeled #{nb_label}"
+    exit
+  end
+end
+
+balancers.each do |nb|
+  puts "Found NodeBalancer #{nb.label} (#{nb.nodebalancerid})"
+end
 
 monitor_nodes = Array.new
 
-config_list = l.nodebalancer.config.list(:NodeBalancerID => nb.nodebalancerid)
-config_list.each do |conf|
-  nodes = l.nodebalancer.node.list(:ConfigID => conf.configid)
-  nodes.each do |node|
-    if node.label == cmd_label
-      puts "found node on port #{conf.port}, changing status to #{cmd_action}"
-      l.nodebalancer.node.update(:NodeID => node.nodeid, :Mode => cmd_action)
-      puts "  status changed to #{cmd_action}"
-      monitor_nodes << {:config => conf, :node_id => node.nodeid}
+balancers.each do |nb|
+  config_list = l.nodebalancer.config.list(:NodeBalancerID => nb.nodebalancerid)
+  config_list.each do |conf|
+    nodes = l.nodebalancer.node.list(:ConfigID => conf.configid)
+    nodes.each do |node|
+      if node.label == cmd_label
+        puts "found node on #{nb.label} ext port #{conf.port}, changing status to #{cmd_action}"
+        l.nodebalancer.node.update(:NodeID => node.nodeid, :Mode => cmd_action)
+        puts "  status changed to #{cmd_action}"
+        monitor_nodes << {:config => conf, :node_id => node.nodeid}
+      end
     end
   end
 end
